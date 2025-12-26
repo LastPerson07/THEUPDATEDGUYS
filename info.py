eb3e8f0ba436378da5c00a0a9f63f0d865b47ece
LastPerson07 <sharmadhanpal950@gmail.com>
2025-12-26 19:08:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -123,7 +123,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/THEUPDATEDGUYS_Support') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Public_Movie_Support') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/DmOwner') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/THEUPDATEDGUYS') # Update channel link for the bot
 
